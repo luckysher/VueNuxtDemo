2061cf84cfe593d0be2d53152311daf557494d51
luckysher <luckyshera95@gmail.com>
2018-03-21 02:05:50 +0530

    updated modal and added data export stmt

---
+++ components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Modal -->
+  <div id="demoModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content" style="background-color: cadetblue;">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">{{ modalHeading }}</h4>
+        </div>
+        <div class="modal-body">
+          <p>{{ content }}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return { modalHeading: 'Demo modal',
+      content: 'This is demo modal for VUE and NUXT project'
+    }
+  }
+}
+</script>
