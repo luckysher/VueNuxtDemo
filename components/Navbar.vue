5d80d0dab3c8c5aa4b8b22e05d899c134535d0ad
luckysher <luckyshera95@gmail.com>
2018-03-21 01:30:37 +0530

    added demo modal

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
   <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">VueNuxtDemo</a>
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">VueNuxtDemo</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Menu 1</a></li>
+        <li><a href="#">Menu 2</a></li>
+        <li><a href="#">Menu 3</a></li>
+      </ul>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Menu 1</a></li>
-      <li><a href="#">Menu 2</a></li>
-      <li><a href="#">Menu 3</a></li>
-    </ul>
-  </div>
-</nav>
+  </nav>
 </template>
