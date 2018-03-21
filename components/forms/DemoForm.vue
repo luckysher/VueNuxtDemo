8f911560d85745bcab7a44918fcf0e683ccc027d
luckysher <luckyshera95@gmail.com>
2018-03-21 11:15:44 +0530

    updated demo form

--- components/forms/DemoForm.vue
+++ components/forms/DemoForm.vue
@@ -1,16 +1,24 @@
 <template>
-  <form>
-    <div class="form-group">
-      <label for="email">Email address:</label>
-      <input type="email" class="form-control" id="email">
-    </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd">
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox"> Remember me</label>
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-  </form>
+  <div>
+    <form>
+      <div class="form-group">
+        <label for="name">Username:</label>
+        <input type="text" v-model="form.username" class="form-control" id="name" >
+      </div>
+      <div class="form-group">
+        <label for="email">Email address:</label>
+        <input type="email" v-model="form.email" class="form-control" id="email">
+      </div>
+      <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" v-model="form.password" class="form-control" id="pwd" >
+      </div>
+      <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#demoModal" v-on:click="send($event)">Open Modal</button>
+    </form>
+  </div>
 </template>
+<script>
+export default {
+
+}
+</script>
