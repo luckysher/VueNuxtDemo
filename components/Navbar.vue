6ffb5bec6918b0bcee8e725aa6b864ca424c8b3b
luckysher <luckyshera95@gmail.com>
2018-03-23 01:17:52 +0530

    updated menu name

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,7 @@
       </div>
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Menu 1</a></li>
+        <li><a href="#imgdemo">Img Demo</a></li>
         <li><a href="#">Menu 2</a></li>
         <li><a href="#">Menu 3</a></li>
       </ul>
