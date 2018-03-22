4ac051583797995b78dc5a37e0251c233fed2800
luckysher <luckyshera95@gmail.com>
2018-03-23 02:28:24 +0530

    added click fun

--- static/js/demo.js
+++ static/js/demo.js
@@ -1,8 +1,21 @@
 
 $(document).ready(function(){
+  var ac = null;
+  var h = function(o){
+    if($(o).hasClass('show')){
+        $(o).removeClass('show');
+    }
+  }
   $('.navbar').find('ul li a').bind('click', function(){
     if($(this).attr('href') === '#imgdemo'){
-      $('#form').css('display', 'none');
+        h(ac);
+        $('.demoImg').addClass('show');
+        ac = $('demoImg');
+    }
+    if($(this).attr('href') === '#formdemo'){
+        h(ac);
+        $('#form').addClass('show');
+        ac = $('#form');
     }
         alert("testing jjquery load..." + $(this).attr('href'));
 });});
