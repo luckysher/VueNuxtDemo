c779796257c222369fdcc73af46c183bf09c2b99
luckysher <luckyshera95@gmail.com>
2018-03-23 10:15:56 +0530

    updated v-for demo

--- pages/index.vue
+++ pages/index.vue
@@ -6,26 +6,28 @@
       <div style="height:84vh;">
         <div class="demoImg">
         </div>
-        <div class="vfor">
-          <table>
-            <thead>
-            <tr><th width="10%">Sr.No</th><th width="20%">Title</th><th width="10%">Author</th><th width="60%">Description</th></tr>
-            </thead>
-            <tbody>
-            <tr v-for="(book, index) in books">
-              <td>{{ index }}</td><td>{{ book.name }}</td><td>{{ book.author }}</td><td> {{ book.description }}</td>
-            </tr>
-            </tbody>
-          </table>
+        <div id="vfor"  style="display:none;" class="show">
+          <h3>v-for loop demo</h3>
+          <div class="vfor">
+            <table class="table">
+              <thead>
+              <tr class="warning"><th width="10%">Sr.No</th><th width="20%">Title</th><th width="10%">Author</th><th width="60%">Description</th></tr>
+              </thead>
+              <tbody>
+              <tr v-for="book in books">
+                <td>{{ index }}</td><td>{{ book.name }}</td><td>{{ book.author }}</td><td> {{ book.description }}</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <!-- Page body end -->
+    <!-- Page footer End -->
   </div>
 </template>
 
 <script>
-export default {
   data: {
     return {
       books:[
