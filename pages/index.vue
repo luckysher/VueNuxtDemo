d4922cb8b37251475a84bbf0d6d0d60f765d05a6
luckysher <luckyshera95@gmail.com>
2018-03-23 10:04:09 +0530

    added vfor demo

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,21 @@
       <div style="height:84vh;">
         <div class="demoImg">
         </div>
+        <div class="vfor">
+          <table>
+            <thead>
+            <tr><th width="10%">Sr.No</th><th width="20%">Title</th><th width="10%">Author</th><th width="60%">Description</th></tr>
+            </thead>
+            <tbody>
+            <tr v-for="(book, index) in books">
+              <td>{{ index }}</td><td>{{ book.name }}</td><td>{{ book.author }}</td><td> {{ book.description }}</td>
+            </tr>
+            </tbody>
+          </table>
       </div>
     </div>
-    <!-- Page footer End -->
+  </div>
+  <!-- Page body end -->
   </div>
 </template>
 
